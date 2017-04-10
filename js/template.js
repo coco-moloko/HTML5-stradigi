fc9544cc8342a5f319c3637de8542c2b804a1022
Coco Moloko <courtneyb.brecht@gmail.com>
2017-04-10 01:34:09 +0000

    first commit

---
+++ js/template.js
@@ -0,0 +1,26 @@
+// hamburger menu toggle
+
+$('html').addClass('menu');
+    $(document).ready(function() {
+      $('menu').show();  // EDIT: Can also use $('html').removeClass('hidden'); 
+    });
+
+
+$(document).ready(function() {
+    $( ".cross" ).hide();
+        $( ".menu" ).hide();
+            $( ".hamburger" ).click(function() {
+                $( ".menu" ).slideToggle( "fast", function() {
+                $( ".hamburger" ).hide();
+                $( ".cross" ).show();
+        });
+    });
+    
+    $( ".cross" ).click(function() {
+        $( ".menu" ).slideToggle( "fast", function() {
+            $( ".cross" ).hide();
+            $( ".hamburger" ).show();
+        });
+    });
+
+});
